1bdf93f5958a123a5bfd134a6e51c19cf0404625
Bharat Singh <74733419+bharats9768@users.noreply.github.com>
2021-10-12 02:18:41 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -4,37 +4,46 @@ import { useState } from "react";
 
 const movieList = {
   Scifi: [
-    { name: "Inception", rating: "4.5/5" },
-    { name: "interstelar", rating: "5/5" },
-    { name: "Pacific Rim", rating: "3.5/5" }
+    {
+      name: "Inception",
+      rating: "4.5/5",
+      description:
+        "Inception is a science fiction action film. The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious of his targets."
+    },
+    {
+      name: "interstellar",
+      rating: "5/5",
+      description:
+        "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans. "
+    }
   ],
 
   Animation: [
     {
       name: "Up",
-      rating: "5/5"
+      rating: "5/5",
+      description:
+        "78-year-old Carl Fredricksen travels to Paradise Falls in his house equipped with balloons, inadvertently taking a young stowaway. ... Determined to save his home and keep the promise he made to his wife, widower Carl Fredricksen embarks on a journey to the mysterious Paradise Falls in an airship of his own invention."
     },
     {
-      name: "wreck it ralph",
-      rating: "4.5/5"
-    },
-    {
-      name: "Toy story",
-      rating: "5/5"
+      name: "Wreck it ralph",
+      rating: "4.5/5",
+      description:
+        "Ralph is a heavy-handed wrecking' riot with a heart. For 30 years—day in, day out—he's been doing his job as The Bad Guy in the arcade game Fix-it Felix Jr. But it's getting harder and harder to love his job when no one seems to like him for doing it."
     }
   ],
   Drama: [
     {
-      name: "Forest gump",
-      rating: "5/5"
+      name: "Forrest gump",
+      rating: "5/5",
+      description:
+        "Forrest Gump is a simple man with a low I.Q. but good intentions. He is running through childhood with his best and only friend Jenny. His 'mama' teaches him the ways of life and leaves him to choose his destiny."
     },
     {
       name: "Shutter island",
-      rating: "5/5"
-    },
-    {
-      name: "Catch me if you can!",
-      rating: "4/5"
+      rating: "5/5",
+      description:
+        "In 1954, a U.S. Marshal investigates the disappearance of a murderer who escaped from a hospital for the criminally insane. In 1954, up-and-coming U.S. marshal Teddy Daniels is assigned to investigate the disappearance of a patient from Boston's Shutter Island Ashecliffe Hospital, But in the end it get more twisted"
     }
   ]
 };
@@ -50,7 +59,7 @@ export default function App() {
     <div className="App">
       <h1> Good movies </h1>
       <p> Check out one of my favourite movies. </p>
-      <p>Click on the different category to get started. </p>
+      <p>Click on the different category from below to get started. </p>
 
       <div>
         {Object.keys(movieList).map((film) => (
@@ -93,6 +102,10 @@ export default function App() {
                 Movie: {movie.name}
               </div>
               <div style={{ fontSize: "large" }}> Ratings: {movie.rating} </div>
+              <div style={{ fontSize: "large", margin: "1rem" }}>
+                {" "}
+                {movie.description}{" "}
+              </div>
             </li>
           ))}
         </ul>
